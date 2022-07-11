7095cd053129c791f37aa78e2f2c1a3ad46f9d7a
Rodrigo Sakae <devsakae@gmail.com>
2022-07-11 10:15:54 -0300

    Organizando pasta

--- tarefas-infantis/script.js
+++
@@ -1,5 +0,0 @@
-const saudacao = document.querySelector('#saudacao');
-const msg1 = document.querySelector('#msg1');
-const msg2 = document.querySelector('#msg2');
-const startBtn = document.querySelector('#start');
-
